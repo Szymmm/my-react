5554287cb2fb7f3a0af0cb7875351359b0034b90
Szymon Kowalke <SKowalk@rockwellautomation.com>
2020-02-28 11:58:13 +0100

    Showing groups

--- src/app/components/Dashboard.jsx
+++ src/app/components/Dashboard.jsx
@@ -4,5 +4,18 @@ import React from 'react';
 export const Dashboard = ({groups})=>(
     <div>
         <h2>Dashboard</h2>
+        {groups.map(group=>(
+        <div>
+            {group.name}
+        </div>
+            ))}
     </div>
-)
+);
+
+function mapStateToProps(state) {
+    return {
+        groups:state.groups
+    }
+}
+
+export const ConnectedDashboard = connect(mapStateToProps) (Dashboard);
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -2,8 +2,10 @@ import { store } from './store';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Dashboard } from './components/Dashboard';
+import { Main } from './components/Main'
+//import { Dashboard } from './components/Dashboard';
 
-ReactDOM.render(<Dashboard/>,
+ReactDOM.render(
+    <Main/>,
     document.getElementById("app")
 );
