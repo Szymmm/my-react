8b9891f876b140e17b0b30a01b4e46377a924225
Szymon Kowalke <SKowalk@rockwellautomation.com>
2020-03-03 21:59:12 +0100

    More task details

--- src/app/components/TaskDetail.jsx
+++ src/app/components/TaskDetail.jsx
@@ -1,17 +1,44 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const TaskDetail = ({
     id,
     comments,
     task,
-    isComplete
+    isComplete,
+    groups
 }) => (
     <div>
-        Task details
+        <h2>
+            {task.name}
+        </h2>
+        <button>
+            Complete / Reopen task
+        </button>
+
+        <select>
+            {groups.map(group=>(
+                <option key={group.id} value={group.id}>{group.name}</option>
+            ))}
+        </select>
+        <Link to="/dashboard">
+            <button>Done</button>
+        </Link>
     </div>
 );
 
-const mapStateToProps = state => state;
+const mapStateToProps = (state, ownProps)=>{
+    let id = ownProps.match.params.id;
+    let task = state.tasks.find(task=>task.id===id);
+    let groups = state.groups;
+
+    return {
+        id,
+        task,
+        groups,
+        isComplete:task.isComplete
+    }
+}
 
 export const ConnectedTaskDetail = connect(mapStateToProps)(TaskDetail);
