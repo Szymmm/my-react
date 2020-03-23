59c472757045cfda43424a55c66e73f31bf62e91
Szymon Kowalke <SKowalk@rockwellautomation.com>
2020-03-23 20:18:09 +0100

    Add Login Route Guards

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../store'
-import {ConnectedDashboard} from './Dashboard'
+import { ConnectedDashboard } from './Dashboard'
+import {ConnectedLogin} from './Login'
 import { Router, Route } from 'react-router-dom';
 import { history } from '../store/history';
 import { ConnectedNavigation } from './Navigation'
 import { ConnectedTaskDetail } from './TaskDetail'
+import { Redirect } from 'react-router';
+
+const RouteGuard = Component => ({match})=>
+!store.getState().session.authenticated ? <Redirect to="/"/> : <Component match={match}/>;
 
 export const Main = ()=>(
     <Router history={history}>
         <Provider store={store}>
             <div>
                 <ConnectedNavigation/>
-                {/*<ConnectedDashboard/>*/}
+                <Route exact path="/" component={ConnectedLogin} />
                 <Route
                     exact 
                     path="/dashboard" 
-                    render={()=>(<ConnectedDashboard/>)} 
+                    render={RouteGuard(ConnectedDashboard)} 
                 />
                 <Route
                 exact
                 path="/task/:id"
-                render={({match})=>(<ConnectedTaskDetail match={match} />)}/>
+                render={RouteGuard(ConnectedTaskDetail)}/>
             </div>
         </Provider>
     </Router>
